4de37550118e9c9489b2c3cfe9176e8d4c5e5675
imranjeetsingh <ranjee970@gmail.com>
2018-04-24 01:45:50 +0530

    Added the error message ot show the user when he tries to submit before or after the contest

--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,11 @@
+import time
+import pytz
+from datetime import datetime
+from datetime import date
+from datetime import time
+from datetime import timedelta
+
+from django.utils.timezone import localtime, now
 from django.views.generic import ListView, DetailView
 from django.http import Http404
 from django.db.models import Q, Max
@@ -5,20 +13,37 @@ from django.db.models import Q, Max
 from .models import Question
 from judge.mixins import LoginRequiredMixin
 from grader.models import Solution
-
+from grader.util import start_time
 
 class QuestionListView(LoginRequiredMixin, ListView):
     template_name = 'questions/list.html'
-    
+      # activate(settings.TIME_ZONE)
+    context_object_name='alert'
+
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(QuestionListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        current_time = datetime.now()
+
+        if current_time <=start_time:
+            context['error'] = 'Contest has not started yet'
+        return context
     def get_queryset(self):
+        current_time = datetime.now()
+    
+        if current_time <=start_time:
+            self.template_name='home.html'
         questions = Question.objects.all()
         user = self.request.user
         for ques in questions:
-            marks = Solution.objects.filter(Q(user=user) & Q(question=ques)).aggregate(Max('score'))
+            marks = Solution.objects.filter(Q(user=user) & Q(
+                question=ques)).aggregate(Max('score'))
             if marks['score__max'] is None:
                 ques.marks_obtained = 0
             else:
-                ques.marks_obtained=marks['score__max']
+                ques.marks_obtained = marks['score__max']
         return questions
 
 
