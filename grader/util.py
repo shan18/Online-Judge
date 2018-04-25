1287b7210c464e82bb858ece72dac821f2a2d895
imranjeetsingh <ranjee970@gmail.com>
2018-04-25 19:50:59 +0530

    improved contact page

--- grader/util.py
+++ grader/util.py
@@ -5,6 +5,6 @@ from datetime import date
 from datetime import time
 from datetime import timedelta
 
-start_time=datetime(2018, 4, 25, 13, 30, 00)
+start_time=datetime(2018, 4, 25, 18, 30, 00)
 
 passkey= "sunil"
