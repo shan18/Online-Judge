780740d91519d714a5deee801d442efd8f61bf9f
Shantanu <thegeek.004@gmail.com>
2019-04-30 21:12:09 +0530

    switch to sqlite3 in production

--- judge/settings/production.py
+++ judge/settings/production.py
@@ -95,9 +95,10 @@ WSGI_APPLICATION = 'judge.wsgi.application'
 # Database
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
